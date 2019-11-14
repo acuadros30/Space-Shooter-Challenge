3d7b730d092a7ae61623c4c2aea03d11be3a9946
acuadros30 <55261261+acuadros30@users.noreply.github.com>
2019-11-13 23:36:30 -0500

    Add files via upload

---
+++ Scripts/Mover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * speed;
+    }
+}
